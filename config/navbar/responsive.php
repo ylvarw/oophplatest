343330f9ee8c33a05c9be5c5bf0af0cb85a391d9
ylvarw <ylvarw@gmail.com>
2020-04-16 19:37:04 +0200

    Add files via upload

---
+++ config/navbar/responsive.php
@@ -0,0 +1,88 @@
+<?php
+/**
+ * Supply the basis for the navbar as an array.
+ */
+return [
+    // Use for styling the menu
+    "id" => "rm-menu",
+    "wrapper" => null,
+    "class" => "rm-default rm-mobile",
+
+    // Here comes the menu items
+    "items" => [
+        [
+            "text" => "Hem",
+            "url" => "",
+            "title" => "Första sidan, börja här.",
+        ],
+        [
+            "text" => "Redovisning",
+            "url" => "redovisning",
+            "title" => "Redovisningstexter från kursmomenten.",
+            "submenu" => [
+                "items" => [
+                    [
+                        "text" => "Kmom01",
+                        "url" => "redovisning/kmom01",
+                        "title" => "Redovisning för kmom01.",
+                    ],
+                    [
+                        "text" => "Kmom02",
+                        "url" => "redovisning/kmom02",
+                        "title" => "Redovisning för kmom02.",
+                    ],
+                    [
+                        "text" => "Kmom03",
+                        "url" => "redovisning/kmom03",
+                        "title" => "Redovisning för kmom03.",
+                    ],
+                    [
+                        "text" => "Kmom04",
+                        "url" => "redovisning/kmom04",
+                        "title" => "Redovisning för kmom04.",
+                    ],
+                    [
+                        "text" => "Kmom05",
+                        "url" => "redovisning/kmom05",
+                        "title" => "Redovisning för kmom05.",
+                    ],
+                    [
+                        "text" => "Kmom06",
+                        "url" => "redovisning/kmom06",
+                        "title" => "Redovisning för kmom06.",
+                    ],
+                    [
+                        "text" => "Kmom07-10",
+                        "url" => "redovisning/kmom07-10",
+                        "title" => "Redovisning för kmom07-10.",
+                    ],
+                ],
+            ],
+        ],
+        [
+            "text" => "Om",
+            "url" => "om",
+            "title" => "Om denna webbplats.",
+        ],
+        [
+            "text" => "Styleväljare",
+            "url" => "style",
+            "title" => "Välj stylesheet.",
+        ],
+        [
+            "text" => "Docs",
+            "url" => "dokumentation",
+            "title" => "Dokumentation av ramverk och liknande.",
+        ],
+        [
+            "text" => "Test &amp; Lek",
+            "url" => "lek",
+            "title" => "Testa och lek med test- och exempelprogram",
+        ],
+        [
+            "text" => "Anax dev",
+            "url" => "dev",
+            "title" => "Anax development utilities",
+        ],
+    ],
+];
